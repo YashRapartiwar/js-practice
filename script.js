c0b2473c9509e8d912410a2e57b503bdbfd2a94f
YashRapartiwar <yrapartiwar7058@gmail.com>
2026-06-25 13:13:00 +0530

    Added mouseover and mouseout event listeners

--- script.js
+++ script.js
@@ -1,42 +1,10 @@
- let form = document.querySelector("form");
- let inputs = document.querySelectorAll("input");
+let abcd = document.querySelector("#abcd");
 
- let main = document.querySelector("#main");
- form.addEventListener("submit",function (dets){
-    dets.preventDefault();
+abcd.addEventListener("mouseover", function (){
+  abcd.style.backgroundColor = "yellow";
 
-  let card = document.createElement("div");
-  card.classList.add("card");
+});
+abcd.addEventListener("mouseout", function (){
+  abcd.style.backgroundColor = "red";
 
-  let profile = document.createElement("div");
-  profile.classList.add("profile");
-    let img = document.createElement("img");
-    img.setAttribute("src",
-        inputs[0].value);
-let h3 = document.createElement("h3");
-h3.textContent = inputs[1].value;
-let h5 = document.createElement("h5");
-h5.textContent = inputs[2].value;
-let p = document.createElement("p");
-p.textContent=inputs[3].value;
-profile.appendChild(img);
-        card.appendChild(profile);
-
-        card.appendChild(h3);
-        card.appendChild(h5);
-        card.appendChild(p);
-
-
-
-        main.appendChild(card);
-        inputs.forEach(function (inp){
-            if(inp.type!=="submit"){
-
-            
-            inp.value = "";
-            }
-        })
-
-
-
- });
+});
