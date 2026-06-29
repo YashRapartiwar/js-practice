4ce6b159d07dd74953956df5817ebef6d4bf3834
YashRapartiwar <yrapartiwar7058@gmail.com>
2026-06-29 17:36:23 +0530

    Event Capturing

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let ul=document.querySelector("ul");
+ let inp=document.querySelector("input");
+ let span=document.querySelector("span");
+inp.addEventListener("input", fucntion(){
+    span.textContent=inp.value.length;
+    
 
-ul.addEventListener("click" , function(dets){
-   dets.target.classList.toggle("lt");
 });
